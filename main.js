ad241055c62a5dcb096a02d088e6a687649835fe
VictorLima <victoraugustolima1@gmail.com>
2022-09-15 19:32:01 -0300

    EDITAR TREINO E DELETAR

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ const acessarDados = ()=>
          carga: carga.value
     })
     localStorage.setItem("dados", JSON.stringify(dados))
-    console.log(dados)
+
     criarExercicio()
 }
 
@@ -45,6 +45,10 @@ const criarExercicio = ()=>
             <li class="lista__item">${x.exercicio}</li>
             <li class="lista__item">${x.series}</li>
             <li class="lista__item">${x.carga}</li>
+            <div class="icones">
+                    <i onClick="editar(this)" class="fa-regular fa-pen-to-square"></i>
+                    <i onClick="deletar(this), criarExercicio()"class="fa-solid fa-trash"></i>
+                </div>
     </ul>`
     })
 
@@ -62,3 +66,25 @@ const resetarForm = ()=>
     dados = JSON.parse(localStorage.getItem("dados")) || []
     criarExercicio();
 })()
+
+const deletar = (e)=>
+{
+
+    e.parentElement.parentElement.remove()  
+    dados.splice(e.parentElement.parentElement.id, 1)
+
+    localStorage.setItem("dados", JSON.stringify(dados))
+  console.log(dados)
+
+}
+
+const editar = (e) =>
+{
+    let treinoSelecionado = e.parentElement.parentElement
+
+    exercicio.value = treinoSelecionado.children[0].textContent
+    series.value = treinoSelecionado.children[1].textContent
+    carga.value = treinoSelecionado.children[2].textContent
+
+    deletar(e)
+}
